a921b1343532b0b33340677bcddba460e27c3cae
theneek14 <neel.19je0558@mc.iitism.ac.in>
2022-10-10 22:50:44 +0530

    added interviewbit backend

--- backend/config/urlList.js
+++ backend/config/urlList.js
@@ -11,6 +11,7 @@ const urlList = {
 	pinterest: 'https://in.pinterest.com/',
 	instagram: 'https://www.instagram.com/',
 	behance: 'https://www.behance.net/',
+	interviewbit: 'https://www.interviewbit.com/profile/',
 };
 
 module.exports = urlList;
